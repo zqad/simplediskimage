bf2356ef600f8be07c0d6f25fb83bcebe4970c36
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 00:22:46 +0000

    Adding tarfile member sanitization to extractall()

--- examples/_rootfs-in-p2.py
+++ examples/_rootfs-in-p2.py
@@ -52,7 +52,26 @@ def main():
     rootfs_tar = os.path.join(temp_dir, "rootfs.tar")
     rootfs_dir = os.path.join(temp_dir, "p2-rootfs-dir")
     with tarfile.open(rootfs_tar, 'r:') as tf:
-        tf.extractall(rootfs_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, rootfs_dir)
 
     # Use the rootfs directory as the initial data directory for the second
     # partition (the rootfs)
